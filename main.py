1fab1a5ea3f4981af5ffa2b6f5a57c4b780ba03c
BrunoFgR <brunofds281@gmail.com>
2025-03-19 21:37:13 -0300

    create asteroid instance and add it in the game

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+from ast import AST
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawables)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawables)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while(True):
@@ -17,10 +32,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawables:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0
 
