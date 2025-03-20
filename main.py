11823611b5fdcd2f4726ac9c976d0f43ed34e812
BrunoFgR <brunofds281@gmail.com>
2025-03-19 22:08:24 -0300

    add shot instance and method

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import *
 from player import Player
+from shot import Shot
 
 def main():
     pygame.init()
@@ -13,8 +14,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+
 
     Asteroid.containers = (asteroids, updatable, drawables)
+    Shot.containers = (shots, updatable, drawables)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 
